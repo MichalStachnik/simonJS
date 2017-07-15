8a2943013b853d168037332446fda6fc1be22387
Michal <Michal.stachnik@my.liu.edu>
2017-07-15 16:50:48 -0400

    working! :D

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const start = document.getElementById('start')
 
 var randomArray= []
 var playerArray = []
-var isPlaying = false
 for(var i = 0; i < 20; i++){
     randomArray[i] = Math.ceil(Math.random() * 4)
 }
@@ -35,61 +34,62 @@ four.onclick = function(){
     console.log(playerArray)
 }
 computerTurn = function(currentRound){
-    for(var i = 0; i < currentRound; i++){
-        console.log('what we should be flashing', randomArray[i])
-        switch(randomArray[i]){
-            case 1:
-                one.classList.toggle('active')
-                setTimeout(function(){one.classList.toggle('active')}, 800)
-                break
-            case 2:
-                two.classList.toggle('active')
-                setTimeout(function(){two.classList.toggle('active')}, 800)
-                break
-            case 3:
-                three.classList.toggle('active')
-                setTimeout(function(){three.classList.toggle('active')}, 800)
-                break
-            case 4:
-                four.classList.toggle('active')
-                setTimeout(function(){four.classList.toggle('active')}, 800)
-                break
-        }
-    }
-}
-
-// playerTurn = function()
-start.onclick = function(){
-    var round = 2
     playerArray = []
-    console.log('round', round)
-    console.log(randomArray)
-    computerTurn(round)
-    
+    var flashInterval = setInterval(flashFunction, 1000)
+    var i = 0
+    function flashFunction(){
+        if(i < currentRound){
+            switch(randomArray[i]){
+                case 1:
+                    one.classList.toggle('active')
+                    setTimeout(function(){one.classList.toggle('active')}, 800)
+                    break
+                case 2:
+                    two.classList.toggle('active')
+                    setTimeout(function(){two.classList.toggle('active')}, 800)
+                    break
+                case 3:
+                    three.classList.toggle('active')
+                    setTimeout(function(){three.classList.toggle('active')}, 800)
+                    break
+                case 4:
+                    four.classList.toggle('active')
+                    setTimeout(function(){four.classList.toggle('active')}, 800)
+                    break
+            }
+        }
+        i++
+    }
+    playerTurn(currentRound)
+}
+playerTurn = function(currentRound){
+    playerArray = []
+    var isSame
     var checking = setInterval(checkInput, 1000)
-
     function checkInput(){
-        if(playerArray.length != round){
+        if(playerArray.length != currentRound){
             console.log('waiting for more input')
         }
         else{
             console.log('checking input')
-            var isSame
-            for(var i = 0; i < round; i++){
+            for(var i = 0; i < currentRound; i++){
                 if(playerArray[i] === randomArray[i]){
                     console.log('same', playerArray[i], randomArray[i])
+                    console.log('should go to next round')
+                    clearInterval(checking)
+                    currentRound++
+                    computerTurn(currentRound)
                 }
                 else{
-                    isSame = false
+                    console.log('should repeat this round')
+                    return currentRound
                 }
-            }
-            if(isSame){
-                console.log('we should go to next round')
-            }
-            else{
-                console.log('something wrong')
             }
         }
     }
 }
-
+start.onclick = function(){
+    var round = 1
+    console.log(randomArray)
+    computerTurn(round)
+}
